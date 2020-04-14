484e02bc0cbd8f0b49b11fa6bac8d1185d650dc1
Nathan Martel <nathanmartel@gmail.com>
2020-04-13 21:37:41 -0700

    more tests passing, now for the fun stuff...

--- e01-date-adder/_tests_/dateAdder.test.js
+++ e01-date-adder/_tests_/dateAdder.test.js
@@ -12,5 +12,30 @@ describe('Check dates', () => {
     const actual = dateAdder(date, diff);
     expect(actual).toEqual(expected);
   });
+
+  it('Should add 10 minutes', () => {
+    const date = new Date("04/13/2020");
+    const diff = '10m';
+    const expected = new Date("2020-04-13T07:10:00.000Z");
+    const actual = dateAdder(date, diff);
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should add 10 hours', () => {
+    const date = new Date("04/13/2020");
+    const diff = '10h';
+    const expected = new Date("2020-04-13T17:00:00.000Z");
+    const actual = dateAdder(date, diff);
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should add 10 days', () => {
+    const date = new Date("04/13/2020");
+    const diff = '10d';
+    const expected = new Date("2020-04-23T07:00:00.000Z");
+    const actual = dateAdder(date, diff);
+    expect(actual).toEqual(expected);
+  });
+
 });
 
