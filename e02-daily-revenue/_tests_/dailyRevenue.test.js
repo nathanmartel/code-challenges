01835586671d3788b330e12fd4afb8e612981543
Nathan Martel <nathanmartel@gmail.com>
2020-04-14 18:00:30 -0700

    init

---
+++ e02-daily-revenue/_tests_/dailyRevenue.test.js
@@ -0,0 +1,12 @@
+const dateAdder = require('../dailyRevenue.js')
+
+describe('Check revenue', () => {
+
+  it('Should be true', () => {
+    const expected = true;
+    const actual = true;
+    expect(actual).toEqual(expected);
+  });
+
+});
+
