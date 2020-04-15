dbdeaa14d9d3eec8aad5db8907ff3e2d6cc0a668
Nathan Martel <nathanmartel@gmail.com>
2020-04-14 20:51:28 -0700

    appears to be working

--- e02-daily-revenue/_tests_/dailyRevenue.test.js
+++ e02-daily-revenue/_tests_/dailyRevenue.test.js
@@ -1,10 +1,12 @@
-const dateAdder = require('../dailyRevenue.js')
+const transactions = require('../transactions.js');
+const dailyRevenue = require('../dailyRevenue.js')
 
 describe('Check revenue', () => {
 
   it('Should be true', () => {
     const expected = true;
-    const actual = true;
+    const actual = dailyRevenue(transactions);
+    console.log('Results:', actual);
     expect(actual).toEqual(expected);
   });
 
--- e02-daily-revenue/dailyRevenue.js
+++ e02-daily-revenue/dailyRevenue.js
@@ -1,8 +1,22 @@
-const transactions = require('transactions');
-
-module.exports = () => {
+module.exports = (transactions) => {
 // Convert transactions timestamps to short dates (toLocaleDateString()?)
 // Reduce() transactions to new array of objects, summing prices if date exists in new array
+
+const dailyTransactions = [];
+transactions.forEach(item => {
+  const date = new Date(item.timestamp)
+  if (dailyTransactions[date.toLocaleDateString()]) {
+    console.log(date.toLocaleDateString(),':', dailyTransactions[date.toLocaleDateString()]);
+    console.log('item.price:', item.price);
+    dailyTransactions[date.toLocaleDateString()] += item.price;
+    console.log(date.toLocaleDateString(),':', dailyTransactions[date.toLocaleDateString()]);
+    console.log('---')
+  }
+  else dailyTransactions[date.toLocaleDateString()] = item.price;
+});
+
+return dailyTransactions;
+
 // Map through array to create a new object in desired shape
 // Return new object
 }
