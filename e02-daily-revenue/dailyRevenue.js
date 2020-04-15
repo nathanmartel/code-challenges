802c60ef838ef91974e307201daaf059254b55fe
Nathan Martel <nathanmartel@gmail.com>
2020-04-14 18:16:20 -0700

    planning

--- e02-daily-revenue/dailyRevenue.js
+++ e02-daily-revenue/dailyRevenue.js
@@ -1,3 +1,8 @@
+const transactions = require('transactions');
+
 module.exports = () => {
- 
+// Convert transactions timestamps to short dates (toLocaleDateString()?)
+// Reduce() transactions to new array of objects, summing prices if date exists in new array
+// Map through array to create a new object in desired shape
+// Return new object
 }
