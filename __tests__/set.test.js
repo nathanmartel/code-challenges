9ea24633d953ffbdc0b1ffcb72ba3a44181a13b7
Nathan Martel <nathanmartel@gmail.com>
2020-04-25 19:39:34 -0700

    reconfigured code challenges folder structure

---
+++ __tests__/set.test.js
@@ -0,0 +1,9 @@
+describe('Check Set', () => {
+
+  it('Should be true (outputs results)', () => {
+    const actual = true;
+    const expected = true;
+    expect(actual).toEqual(expected);
+  });
+
+});
